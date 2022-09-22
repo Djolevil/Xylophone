3c20573cd9bb2d341a7e10b4a455949a58a130a8
Djordje Knezevic <starwarrior@mycity.rs>
2022-09-22 17:28:36 +0200

    Change opacity of button when pressed and restore it afterwards to simulate button pressed animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -4,14 +4,22 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
         
         playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            
+            sender.alpha = 1.0
+        }
         
     }
     
@@ -20,7 +28,7 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+        
     }
 }
 
